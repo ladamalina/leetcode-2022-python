882fcb818d65f0a22b88fa8a70260d0396f5cbcf
Nadezhda R <nadezhdaryabtsova@gmail.com>
2022-03-05 04:15:10 +0300

    80. Remove Duplicates from Sorted Array II (medium)

--- 80. Remove Duplicates from Sorted Array II (medium)/main.py
+++ 80. Remove Duplicates from Sorted Array II (medium)/main.py
@@ -4,7 +4,7 @@ from typing import List
 
 class Solution:
     @classmethod
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(cls, nums: List[int]) -> int:
         logging.debug(f"input: nums = {nums}")
         last_uniq_num = -99999
         last_uniq_num_count = 0
@@ -29,15 +29,15 @@ class Solution:
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
-    nums1 = [1,1,1,2,2,3]
-    expected_nums1 = [1,1,2,2,3]
+    nums1 = [1, 1, 1, 2, 2, 3]
+    expected_nums1 = [1, 1, 2, 2, 3]
     k1 = Solution.removeDuplicates(nums1)
     assert k1 == len(expected_nums1)
     for i in range(len(expected_nums1)):
         assert nums1[i] == expected_nums1[i]
 
-    nums2 = [0,0,1,1,1,1,2,3,3]
-    expected_nums2 = [0,0,1,1,2,3,3]
+    nums2 = [0, 0, 1, 1, 1, 1, 2, 3, 3]
+    expected_nums2 = [0, 0, 1, 1, 2, 3, 3]
     k2 = Solution.removeDuplicates(nums2)
     assert k2 == len(expected_nums2)
     for i in range(len(expected_nums2)):
