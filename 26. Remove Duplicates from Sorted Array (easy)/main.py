f16076b3d42b80df0717b52ab8d746024b2c36ac
Nadezhda R <nadezhdaryabtsova@gmail.com>
2022-03-13 04:15:10 +0300

    26. Remove Duplicates from Sorted Array (easy)

--- 26. Remove Duplicates from Sorted Array (easy)/main.py
+++ 26. Remove Duplicates from Sorted Array (easy)/main.py
@@ -4,7 +4,7 @@ from typing import List
 
 class Solution:
     @classmethod
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(cls, nums: List[int]) -> int:
         logging.debug(f"input: nums = {nums}")
         last_uniq_num = -1000
         uniq_nums_idx = -1
@@ -23,15 +23,15 @@ class Solution:
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
-    nums1 = [1,1,2]
+    nums1 = [1, 1, 2]
     expected_nums1 = [1, 2]
     k1 = Solution.removeDuplicates(nums1)
     assert k1 == len(expected_nums1)
     for i in range(len(expected_nums1)):
         assert nums1[i] == expected_nums1[i]
 
-    nums2 = [0,0,1,1,1,2,2,3,3,4]
-    expected_nums2 = [0,1,2,3,4]
+    nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    expected_nums2 = [0, 1, 2, 3, 4]
     k2 = Solution.removeDuplicates(nums2)
     assert k2 == len(expected_nums2)
     for i in range(len(expected_nums2)):
